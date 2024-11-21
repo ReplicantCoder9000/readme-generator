fb004a69d65db9dd403abb7af5b8607ae63030b3
ReplicantCoder9000 <182562187+ReplicantCoder9000@users.noreply.github.com>
2024-11-20 22:51:16 -0800

    Update generateMarkdown.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,22 +1,75 @@
 // generateMarkdown.js
 
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  function renderLicenseBadge(license) {
+    if (license !== 'None') {
+      return `[![License: ${license}](https://img.shields.io/badge/License-${encodeURIComponent(license)}-blue.svg)](https://opensource.org/licenses/${encodeURIComponent(license)})`;
+    }
+    return '';
+  }
+
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+- [Walkthrough Video](#walkthrough-video)
+- [Code Source](#code-source)
+
+## Installation
+
+\`\`\`bash
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license !== 'None' ? `This project is licensed under the [${data.license}](https://opensource.org/licenses/${encodeURIComponent(data.license)}) license.` : 'This project does not have a license.'}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+\`\`\`bash
+${data.tests}
+\`\`\`
+
+## Questions
+
+If you have any questions about the project, feel free to reach out:
+
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: [${data.email}](mailto:${data.email})
+
+## Walkthrough Video
+
+[Watch the walkthrough video here](${data.videoLink})
+
+## Code Source
+
+All source code for this project is located in the following files:
+
+- \`index.js\`: Main application logic and user prompts.
+- \`generateMarkdown.js\`: Functions to format user input into Markdown.
+- \`package.json\`: Project dependencies and scripts.
 `;
 }
 
-export default generateMarkdown;
+module.exports = generateMarkdown;
